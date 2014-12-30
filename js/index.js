9ba8d0aac42611a29768f2e0f7f45f8877c7691e
jianglin <904880305@qq.com>
2014-12-30 17:55:39 +0800

    jl

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
 // JavaScript Document
+$(function(){
+
+$(".jl_div1c3").mouseover(function(){
+	$(".jl_div1c2_b dt").eq(2).fadeIn(200);
+	$(".jl_div1c2_b dt").eq(1).fadeIn(500);
+	$(".jl_div1c2_b dt").eq(0).fadeIn(1000);	
+	});	
+
+$(".jl_div1").mouseout(function(){
+	
+	$(".jl_div1c2_b dt").eq(2).fadeOut(200);
+	$(".jl_div1c2_b dt").eq(1).fadeOut(500);
+	$(".jl_div1c2_b dt").eq(0).fadeOut(1000);
+	
+	});	
+	
+	
+});
