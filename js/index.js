90e4ca161a0d7fbabc073810ba47c123268f2b9c
jianglin <904880305@qq.com>
2014-12-31 14:16:20 +0800

    jl

--- js/index.js
+++ js/index.js
@@ -2,18 +2,36 @@
 $(function(){
 
 $(".jl_div1c3").mouseover(function(){
+	$(".jl_div1c2_b").show();
 	$(".jl_div1c2_b dt").eq(2).fadeIn(200);
 	$(".jl_div1c2_b dt").eq(1).fadeIn(500);
 	$(".jl_div1c2_b dt").eq(0).fadeIn(1000);	
 	});	
-
-$(".jl_div1").mouseout(function(){
-	
-	$(".jl_div1c2_b dt").eq(2).fadeOut(200);
-	$(".jl_div1c2_b dt").eq(1).fadeOut(500);
-	$(".jl_div1c2_b dt").eq(0).fadeOut(1000);
-	
+$(".jl_div1c2_b").mouseover(function(){
+	$(".jl_div1c2_b").show();
+	$(".jl_div1c2_b dt").show();
+	});
+$(".jl_div1c2_b").mouseout(function(){
+	$(".jl_div1c2_b").hide();
+	$(".jl_div1c2_b dt").hide();
 	});	
 	
 	
+	
+	
+	
+	
 });
+
+function getId(id)
+{
+	return documment.getElementById(id);
+}
+function bianse(a)
+{
+	$(a).css({"border-color":"#F00"});
+}
+function bianse1(a)
+{
+	$(a).css({"border-color":"#E3E7EA"});
+}
