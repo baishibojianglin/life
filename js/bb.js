edad813c5446290cf8640618b6659b2c339d7e46
linbin <git@github.com:baishibojianglin/crw.git>
2015-01-05 09:21:06 +0800

    l_b

--- js/bb.js
+++ js/bb.js
@@ -1,70 +1,11 @@
-
-$(function(){
-		$('.lb_daos06').click(function(){
-			$('.lb_daos017').slideToggle("slow");
-		});
-	
-	});
-//****
-	$(function(){
-		$(".xiadao_03").eq(0).addClass(".zitise1"); 
-		$(".xiadao_03").eq(1).addClass(".zitise1");
-		$(".xiadao_03").eq(2).addClass(".zitise1");
-	});
-	
-	$(function(){
-	
-	if(a==0)
-	{
-		 $(".fa15").eq(1).removeClass("zitise2");
-		 $(".fa15").eq(2).removeClass("zitise2");
-
-	     $(".fa15").eq(0).addClass("zitise1");
-	 
-
-	}
-	
-	if(a==1)
-	{
-		$(".xiadao_03").eq(1).css({"color":"#FF7426"}); 
-		for(i=1;i<=2;i++)
-		{$(".fa16").eq(0).css({"color":"#747474"});}
-		{$(".fa16").eq(2).css({"color":"#747474"});}
-	}
-	
-	if(a==2)
-	{
-		$(".xiadao_03").eq(2).css({"color":"#FF7426"}); 
-		for(i=1;i<=2;i++)
-		{$(".fa16").eq(0).css({"color":"#747474"});}
-		{$(".fa16").eq(1).css({"color":"#747474"});}
-	}
-	
-	});
-	//
-	
-	
-	
+// JavaScript Document
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+$(document).ready(function(){
+  $(".lb_daos20").click(function(){
+    var div=$("lb_daos07");  
+    div.animate({left:'100px'},"slow");
+    div.animate({fontSize:'3em'},"slow");
+  });
+});
