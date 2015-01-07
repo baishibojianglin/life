907df8999138e3db869a6452d8a832d1e65effd5
jianglin <904880305@qq.com>
2015-01-07 09:14:15 +0800

    jl

--- js/2_cloth.js
+++ js/2_cloth.js
@@ -1,4 +1,10 @@
 // JavaScript Document
 $(function(){
-	$(".jl2_divfu1_2 div").fadeTo(1,0.8);
+	$(".jl2_divfu1_2a").fadeTo(1,0.8);
+	$(".jl2_divfu1_2a").mouseover(function(){
+		$(this).css({"color":"#C60"});
+		});
+	$(".jl2_divfu1_2a").mouseout(function(){
+		$(this).css({"color":"white"});
+		});
 	});
