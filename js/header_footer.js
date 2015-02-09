d182a26813326520227efdc0919d68e0862a7cd4
jianglin <904880305@qq.com>
2015-02-09 11:13:03 +0800

    jl

--- js/header_footer.js
+++ js/header_footer.js
@@ -18,9 +18,9 @@ $("#h2 span").eq(0).css({"margin-top":"1px"});
 $("#h2 a").addClass('h1_span2');
 $("#h2 span").eq(4).css({"color":"#999"});
 $("#h2").addClass('bor');
-$("#h4 span").eq(0).css({"margin-top":"1px"});
 $("#h4 span").eq(2).addClass('h1_span1');
-$("#h5 span").eq(0).css({"margin-top":"2px"});
+$("#h6 img").css({'height':'11px'});
+$("#h7 span").eq(1).addClass('h1_span1');
 //城市
 $("#h2 span").eq(1).mouseover(function(){
   $("#h2 dd").eq(0).show();
@@ -55,6 +55,7 @@ $("#h2 dd").eq(1).mouseout(function(){
   $("#h2 dd").eq(1).hide();
   $("#h2").css({"border-color":"transparent","background-color":"#F0F2F5"});
 });
+
 //我的账户
 $("#h4 span").eq(1).mouseover(function(){
   $("#h4 dd").show();
@@ -71,6 +72,23 @@ $("#h4 dd").mouseover(function(){
 $("#h4 dd").mouseout(function(){
   $("#h4 dd").hide();
   $("#h4").css({"border-color":"transparent","background-color":"#F0F2F5"});
+});
+//网站导航
+$("#h7 span").eq(0).mouseover(function(){
+  $("#h7 div").show();
+  $("#h7").css({"border-color":"#CCC","background-color":"#fff"});
+});
+$("#h7 span").eq(0).mouseout(function(){
+  $("#h7 div").hide();
+  $("#h7").css({"border-color":"transparent","background-color":"#F0F2F5"});
+});
+$("#h7 div").mouseover(function(){
+  $("#h7 div").show();
+  $("#h7").css({"border-color":"#CCC","background-color":"#fff"});
+});
+$("#h7 div").mouseout(function(){
+  $("#h7 div").hide();
+  $("#h7").css({"border-color":"transparent","background-color":"#F0F2F5"});
 });
 });
 
